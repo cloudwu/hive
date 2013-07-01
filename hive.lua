1742da3d17d3f09eef829b20ba5871faec5b484b
Cloud Wu <cloudwu@gmail.com>
2013-07-01 20:41:18 +0800

    hive mondule

---
+++ hive.lua
@@ -0,0 +1,12 @@
+local c = require "hive.c"
+
+local system_cell = assert(package.searchpath("hive.system", package.path),"system cell was not found")
+
+local hive = {}
+
+function hive.start(t)
+	local main = assert(package.searchpath(t.main, package.path), "main cell was not found")
+	return c.start(t, system_cell, main)
+end
+
+return hive
